611e93f4393c58ebd0d232184d670de8a00193dc
choikeunyoung <ought119@gmail.com>
2022-08-02 23:09:08 +0900

    프로그래머스 폴더 생성

---
+++ 프로그래머스/숫자_문자열과_영단어.py
@@ -0,0 +1,12 @@
+def soulation(answer):
+    answer = answer.replace("one",'1')
+    answer = answer.replace("two",'2')
+    answer = answer.replace("three",'3')
+    answer = answer.replace("four",'4')
+    answer = answer.replace("five",'5')
+    answer = answer.replace("six",'6')
+    answer = answer.replace("seven",'7')
+    answer = answer.replace("eight",'8')
+    answer = answer.replace("nine",'9')
+    answer = answer.replace("zero",'0')
+    return int(answer)
