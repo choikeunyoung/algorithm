b20d6a36e1b40ffa15d0175c42c0c994ac029f5b
choikeunyoung <ought119@gmail.com>
2022-08-29 23:30:38 +0900

    25192.py

--- 백준/25192.py
+++ 백준/25192.py
@@ -1,18 +1,14 @@
 T = int(input())
 mes_dict = {}
+cnt = 0
 for i in range(1,T+1):
     mes = input()
     if mes == 'ENTER':
-        continue
+        mes_dict = {}
     else:
         if mes in mes_dict:
             mes_dict[mes] += 1
         else:
             mes_dict[mes] = 1
-cnt = 0
-for v in mes_dict.values():
-    if v == 1:
-        cnt +=1
-    else:
-        continue
+            cnt += 1
 print(cnt)
