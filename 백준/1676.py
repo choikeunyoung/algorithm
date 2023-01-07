e29a5c52a99788b916b8b939ec6d0635f43b6a51
choikeunyoung <ought119@gmail.com>
2023-01-08 01:08:32 +0900

    1676.py

---
+++ 백준/1676.py
@@ -0,0 +1,16 @@
+def factory(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factory(num-1)
+
+cnt = str(factory(int(input())))
+check = 0
+
+
+for i in cnt[::-1]:
+    if i == '0':
+        check += 1
+    else:
+        print(check)
+        break
