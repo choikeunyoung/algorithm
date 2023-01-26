d5f847ab8bd5d266408431120d3cddb2ca4adbb5
choikeunyoung <ought119@gmail.com>
2023-01-27 02:18:09 +0900

    1026.py

---
+++ 백준/Silver 4/1026.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+A.sort()
+B.sort(reverse=True)
+
+total = 0
+
+for i in range(N):
+    total += A[i]*B[i]
+print(total)
