55c27776b13268dc1c747d00b331249c4c936814
choikeunyoung <ought119@gmail.com>
2023-01-29 13:05:20 +0900

    2217.py

---
+++ 백준/Silver 4/2217.py
@@ -0,0 +1,25 @@
+import math
+
+N = int(input())
+lope_list = []
+new_lope = []
+
+for _ in range(N):
+    lope_list.append(int(input()))
+
+lope_list.sort(reverse=True)
+
+check = 0
+max_weight = 0
+
+for i in range(N):
+    if check == 0:
+        min_num = lope_list[0]
+        check += 1
+        max_weight = min_num * 1
+    else:
+        if min_num > lope_list[i]:
+            min_num = lope_list[i]
+        if max_weight < min_num*(i+1):
+            max_weight = min_num*(i+1)
+print(max_weight)
