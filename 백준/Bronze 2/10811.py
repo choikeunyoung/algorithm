2e724ba6c1bc437458659e4c632ff0fe42b3c725
choikeunyoung <ought119@gmail.com>
2023-03-06 22:48:57 +0900

    10811.py

---
+++ 백준/Bronze 2/10811.py
@@ -0,0 +1,14 @@
+N,M = map(int,input().split())
+
+num_list = []
+
+for i in range(1,N+1):
+    num_list.append(i)
+
+for _ in range(M):
+    temp1, temp2 = map(int,input().split())
+    temp = num_list[temp1-1:temp2]
+    temp = temp[::-1]
+    num_list[temp1-1:temp2] = temp
+
+print(*num_list)
