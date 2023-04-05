123258644268874fc5e20fd40e729653db4f5496
choikeunyoung <ought119@gmail.com>
2023-04-05 15:26:53 +0900

    9625.py

---
+++ 백준/Silver 5/9625.py
@@ -0,0 +1,13 @@
+def dp():
+    # 정답 리스트
+    ans_list = [[1,0]]
+    # K가 45까지 들어오기때문에
+    for _ in range(45):
+        ans_list.append([ans_list[-1][1],ans_list[-1][0]+ans_list[-1][1]])
+    return ans_list
+
+
+K = int(input())
+ans = dp()
+# 들어온 K의 값 출력
+print(*ans[K])
