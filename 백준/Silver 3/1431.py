cceb763b48bebe31ba294abe65095e43a6581790
choikeunyoung <ought119@gmail.com>
2023-04-21 23:08:49 +0900

    1431.py 문제풀이 실패

--- 백준/Silver 3/1431.py
+++ 백준/Silver 3/1431.py
@@ -7,32 +7,44 @@ for _ in range(N):
 
 for i in range(N-1):
     for j in range(i+1,N):
-        if len(word_list[i]) > len(word_list[j]):
-            temp = word_list[i]
-            word_list[i] = word_list[j]
-            word_list[j] = temp
-        elif len(word_list[i]) == len(word_list[j]):
-            total_1 = 0
-            flag_1 = 0
-            total_2 = 0
-            flag_2 = 0
-            for k in "0123456789":
-                if k in word_list[i]:
-                    total_1 += int(k)
-                elif k not in word_list[i]:
-                    flag_1 += 1
-                if k in word_list[j]:
-                    total_2 += int(k)
-                elif k not in word_list[j]:
-                    flag_2 += 1
-            if (total_1 == 0 and flag_1 == 10) or (total_2 == 0 and flag_2 == 10):
-                if word_list[i] > word_list[j]:
-                    temp = word_list[i]
-                    word_list[i] = word_list[j]
-                    word_list[j] = temp
-            else:
+        i_length = len(word_list[i])
+        j_length = len(word_list[j])
+        if i_length != j_length:
+            if i_length > j_length:
+                temp = word_list[i]
+                word_list[i] = word_list[j]
+                word_list[j] = temp
+        else:
+            num = "0123456789"
+            if not word_list[i].isalpha() and not word_list[j].isalpha():
+                total_1 = 0
+                total_2 = 0
+                for k in word_list[i]:
+                    if k in num:
+                        total_1 += int(k)
+
+                for l in word_list[j]:
+                    if l in num:
+                        total_2 += int(l)
+
                 if total_1 > total_2:
                     temp = word_list[i]
                     word_list[i] = word_list[j]
                     word_list[j] = temp
-print(word_list)
+                elif total_2 == total_1:
+                    if word_list[i] > word_list[j]:
+                        temp = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = temp
+            else:
+                if word_list[i].isalpha() and word_list[j].isalpha():
+                    if word_list[i] > word_list[j]:
+                        temp = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = temp
+                elif not word_list[i].isalpha() and word_list[j].isalpha():
+                    temp = word_list[i]
+                    word_list[i] = word_list[j]
+                    word_list[j] = temp
+for _ in word_list:
+    print(_)
