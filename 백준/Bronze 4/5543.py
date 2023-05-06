72756a9da4e97047f23768357baf1bb3020368ff
choikeunyoung <ought119@gmail.com>
2023-05-06 10:36:33 +0900

    백준/Bronze 4/5543.py

---
+++ 백준/Bronze 4/5543.py
@@ -0,0 +1,13 @@
+sang = int(input())
+jung = int(input())
+ha = int(input())
+cola = int(input())
+sa = int(input())
+
+set_1 = sang + cola
+set_2 = sang + sa
+set_3 = jung + cola
+set_4 = jung + sa
+set_5 = ha + cola
+set_6 = ha + sa
+print(min(set_1,set_2,set_3,set_4,set_5,set_6)-50)
