e2580aede3f240f6cff3a3c51651d7ca99da23a4
choikeunyoung <ought119@gmail.com>
2023-05-09 02:11:45 +0900

    백준/Silver\ 4/1491.py 주석추가

--- 백준/Silver 4/1491.py
+++ 백준/Silver 4/1491.py
@@ -1,13 +1,21 @@
 N,M = map(int,input().split())
-current_x = 0
-current_y = M
-n_check = 0
-m_check = 0
-
-matrix = [[0] * N for _ in range(M)]
-
-while N == 0 and M == 0:
-    if n_check == 0 and m_check == 0:
-        for i in range(N):
-            matrix[current_x][current_y] = 1
-            current_y += 1
+# 각 각의 값이 짝수인지 홀수인지에 따라서 끝값이 결정된다.
+if N%2 == 0 and M%2 == 0:
+    print(min(N,M)//2-1,min(N,M)//2)
+elif N%2 == 0 and M%2 != 0:
+    if N < M:
+        print(N//2-1,N//2)
+    else:
+        print(N-(M//2)-1,M//2)
+elif N%2 != 0 and M%2 == 0:
+    if N > M:
+        print(M//2-1,M//2)
+    else:
+        print(N//2,M-(N//2)-1)
+else:
+    if N == M:
+        print(N//2,M//2)
+    elif N > M:
+        print(N-(M//2)-1, M//2)
+    elif N < M:
+        print(N//2,M-(N//2)-1)
