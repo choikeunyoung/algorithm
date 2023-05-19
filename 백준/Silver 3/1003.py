7f6d173d0422bd680edf5c612a6ae849bacc4416
choikeunyoung <ought119@gmail.com>
2023-05-19 22:48:26 +0900

    1003.py 주석추가 문제풀이

--- 백준/Silver 3/1003.py
+++ 백준/Silver 3/1003.py
@@ -1,14 +1,16 @@
-# def fibonacci(num):
-#     if num == 0:
-#         return 
-#     elif num == 1:
-#         return 1
-#     else:
-#         return fibonacci(num-1)
-n=int(input())
-score=list(map(int,input().split()))
-m=max(score)
-sum=0
-for i in score:
-    sum+=(i/m*100)
-print(sum/n)
+def fibonacci():
+    # 피보나치 함수에서 각각 0과 1이 나오는 횟수
+    num_list = [[1,0],[0,1]]
+    # 최대 40 까지 오기 때문에 미리 리스트를 생성해둔다.
+    for i in range(2,41):
+        num_list.append([num_list[i-1][0]+num_list[i-2][0],num_list[i-1][1]+num_list[i-2][1]])
+    return num_list
+
+T = int(input())
+# 피보나치 수열 함수 생성
+fibo = fibonacci()
+
+for _ in range(T):
+    # 미리 생성해둔 함수에서 인풋값에 따라 
+    num = int(input())
+    print(*fibo[num])
