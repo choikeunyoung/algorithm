fa04f6c2eb071fa9adc06302f83502ff8bea415a
choikeunyoung <ought119@gmail.com>
2023-06-10 23:41:00 +0900

    2371.py GPT 도움으로 다시 짜볼 예정

--- 백준/Silver 3/2371.py
+++ 백준/Silver 3/2371.py
@@ -1,32 +1,25 @@
 N = int(input())
 
-answer_list = []
-length_list = []
-
+file_lengths = []
 for _ in range(N):
-    num_list = list(map(int,input().split()))
+    num_list = list(map(int, input().split()))
     num_length = len(num_list)
-    length_list.append(num_length)
-    answer_list.append(num_list)
+    file_lengths.append(num_list)
 
-max_value = max(length_list)
+max_length = max(map(len, file_lengths))
+min_k = float('inf')
 
-for i in answer_list:
-    if len(i) < max_value:
-        middle_value = max_value - len(i)
-        i[-1] = 0
-        for _ in range(middle_value):
-            i.append(0)
-        i[-1] = -1
+for K in range(1, max_length + 1):
+    unique_sequences = set()
 
-list_length = len(answer_list)
+    for file in file_lengths:
+        if K > len(file):
+            unique_sequences.add(tuple(file + [0] * (K - len(file))))
+        else:
+            unique_sequences.add(tuple(file[:K]))
 
-for j in range(max_value-1):
-    cnt = 0
-    check = []
-    for k in range(list_length):
-        if answer_list[k][j] not in check:
-            check.append(answer_list[k][j])
-    if len(check) == list_length:
-        print(j+1)
+    if len(unique_sequences) == N:
+        min_k = K
         break
+
+print(min_k)
