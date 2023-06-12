ca488fd718c2e274821c38f398b83b7309f324aa
choikeunyoung <ought119@gmail.com>
2023-06-12 21:49:23 +0900

    2346.py 오늘도 해결못함...

--- 백준/Silver 3/2346.py
+++ 백준/Silver 3/2346.py
@@ -1,16 +1,34 @@
 N = int(input())
+flag = N
 num_list = list(map(int,input().split()))
 check_list = list(num_list)
 list_length = len(num_list)
 index = 0
-while check_list:
-    print(num_list.index(check_list[index])+1, end=" ")
-    check = check_list.pop(index) + index
-    list_length -= 1
-    if check > 0:
-        if check > list_length:
-            check -= list_length
-    elif check < 0:
-        check += list_length
-    print(check, index)
-    index = check -1
+while True:
+    check = check_list[index]
+    print(num_list)
+    print(num_list.index(check)+1, end=" ")
+    num_list[index] = 0
+    check_list[index] = 0
+    index += check
+    flag -= 1
+    if flag == 0:
+        break
+    if index >= list_length:
+        index = index - (index//list_length)*list_length
+    elif index < 0:
+        index = index + (abs(index)//list_length)*list_length
+    if check_list[index] == 0:
+        cnt = 0
+        while True:
+            if check > 0:
+                index += 1
+                if index >= list_length:
+                    index -= list_length
+            elif check < 0:
+                index -= 1
+                if abs(index) >= list_length:
+                    index += list_length
+            if check_list[index] != 0 or cnt == N:
+                break
+            cnt += 1
