787c925ae2d4a292a8c1c6fa2b062f18a4b4765f
choikeunyoung <ought119@gmail.com>
2023-06-13 22:48:27 +0900

    2346.py 해결했음!

--- 백준/Silver 3/2346.py
+++ 백준/Silver 3/2346.py
@@ -1,34 +1,30 @@
+# 풍선 갯수
 N = int(input())
-flag = N
+# 풍선에 적힌 번호
 num_list = list(map(int,input().split()))
-check_list = list(num_list)
-list_length = len(num_list)
+# 풍선 번호와 인덱스 저장 리스트
+check_list = []
+
+for i,v in enumerate(num_list):
+    check_list.append((v,i))
+# 인덱스 번호
 index = 0
-while True:
-    check = check_list[index]
-    print(num_list)
-    print(num_list.index(check)+1, end=" ")
-    num_list[index] = 0
-    check_list[index] = 0
-    index += check
-    flag -= 1
-    if flag == 0:
-        break
-    if index >= list_length:
-        index = index - (index//list_length)*list_length
-    elif index < 0:
-        index = index + (abs(index)//list_length)*list_length
-    if check_list[index] == 0:
-        cnt = 0
-        while True:
-            if check > 0:
-                index += 1
-                if index >= list_length:
-                    index -= list_length
-            elif check < 0:
-                index -= 1
-                if abs(index) >= list_length:
-                    index += list_length
-            if check_list[index] != 0 or cnt == N:
-                break
-            cnt += 1
+# 리스트 길이 저장
+list_length = len(num_list)
+# 체크 리스트 존재하지 않을때 까지 반복문시행
+while check_list:
+    # 체크 변수에 리스트 인덱스 번호 추출
+    check = check_list.pop(index)
+    # 리스트 값이 -1 되서 길이 -1
+    list_length -= 1
+    # 리스트 길이를 먼저 -1 해주기 때문에 아래 연산이 맨 마지막 값에 대해서 0으로 나누기 때문에 0이면 1 넣어줌
+    if list_length == 0:
+        list_length = 1
+    # 추출한 값이 양수인 경우 인덱스 번호는 -1을 해줘야 함으로 -1을 한 후  기존의 index 와 더한 후 리스트 길이보다 얼마나 클지 모르기 때문에 리스트 길이의 나머지를 구해줌
+    if check[0] > 0:
+        index = ((check[0] - 1) + index)%list_length
+    # 음수인경우 인덱스와 그냥 더한 후 리스트 길이의 나머지를 구한다
+    elif check[0] < 0:
+        index = (check[0] + index)%list_length
+    # 같이 넣어뒀던 인덱스 번호를 출력한다.
+    print(check[1]+1, end=" ")
