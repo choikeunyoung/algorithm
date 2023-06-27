18878df5b5cb22030a0c502e1e07cf6a3d6635a0
choikeunyoung <ought119@gmail.com>
2023-06-28 01:02:21 +0900

    4344.py 재채점 문제 해결

---
+++ 백준/Bronze 1/4344.py
@@ -0,0 +1,15 @@
+C = int(input())
+
+for i in range(1,C+1):
+    info_list = list(map(int,input().split()))
+    cnt = 0
+    total = 0
+    for i in info_list[1:]:
+        total += i
+    total_avg = total/info_list[0]
+
+    for i in info_list[1:]:
+        if i > total_avg:
+            cnt += 1
+    ans = cnt/info_list[0] * 100
+    print(f"{ans:.3f}%")
