6d1de422308a24ef3d4bd21c063b32db61571175
최근영 <ought119@gmail.com>
2023-07-24 17:18:54 +0900

    3060.py 주석 추가 문제풀이

--- 백준/Silver 5/3060.py
+++ 백준/Silver 5/3060.py
@@ -1,19 +1,35 @@
 T = int(input())
 
 for i in range(T):
+    # 하루 식사 제한
     day_meal = int(input())
+    # 돼지들의 총 식사
     pig_meal = list(map(int,input().split()))
-    one_day_meal = sum(pig_meal)
-    today = 1
-    answer_list = []
-    while one_day_meal <= day_meal:
-        answer_list.append(pig_meal)
-        for j in range(6):
-            pig_meal[j] += (answer_list[-1][j-1] + answer_list[-1][j-3])
-            if j==5:
-                pig_meal[j] += answer_list[-1][0]
-            else:
-                pig_meal[j] += answer_list[-1][j+1]
-        one_day_meal = sum(pig_meal)
-        today += 1
-    print(today)
+    # 일수
+    cnt = 1
+    # 만약 초기 값이 하루 식사치를 넘었을 경우
+    if sum(pig_meal) > day_meal:
+        pass
+    # 그 외의 경우
+    else:
+        # 하루치 식사보다 작거나 같을때까지 반복
+        while sum(pig_meal) <= day_meal:
+            # 홀수 합
+            odd_total = 0
+            # 짝수 합
+            even_total = 0
+            # 짝수 합과 홀수 합을 더하는 식
+            for j in range(1,7):
+                if j % 2 == 0:
+                    even_total += pig_meal[j-1]
+                else:
+                    odd_total += pig_meal[j-1]
+            # 각각의 자리에서 홀수 합과 짝수 합을 더해서 다시 넣음
+            for k in range(6):
+                if k % 2 == 0:
+                    pig_meal[k] += even_total
+                else:
+                    pig_meal[k] += odd_total
+            # 다음날로 넘어가기 때문에 cnt 증가
+            cnt += 1
+    print(cnt)
