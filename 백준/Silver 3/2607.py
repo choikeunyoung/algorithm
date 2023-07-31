e860c794377fe8997e32f7d6b6e384a54c077947
이상협 <geniusky24@naver.com>
2023-07-31 17:03:50 +0900

    2607.py 문제 푸는중

--- 백준/Silver 3/2607.py
+++ 백준/Silver 3/2607.py
@@ -1,61 +1,99 @@
-import copy
-
 N = int(input())
 
 word_list = []
-cnt = 0
+answer = 0
 
 for _ in range(N):
-    word = input()
-    word_list.append(word)
+    word_list.append(input())
 
-first_word = word_list[0]
+check_list = [word_list[0]]
 
-check_list = [first_word]
+for i in range(len(check_list[-1])):
+    start_word = check_list[0][0:i]
+    middle_word = check_list[0][i:i+1]
+    last_word = check_list[0][i+1:]
+    check_list.append(start_word+last_word)
+    check_list.append(start_word+middle_word+middle_word+last_word)
 
-for i in range(len(first_word)):
-    start_word = first_word[:i]
-    middle_word = first_word[i:i+1]
-    last_word = first_word[i+1:]
-    first_add_word = start_word+middle_word+middle_word+last_word
-    second_add_word = start_word+last_word
-    check_list.append(first_add_word)
-    check_list.append(second_add_word)
-answer_list = []
-for i in range(1,N):
-    word_length = len(word_list[i])
+flag = False
+for i in check_list:
     word_dict = {}
-    for k in word_list[i]:
-        if k in word_dict:
-            word_dict[k] += 1
+    word_length = len(i)
+    for j in i:
+        if j in word_dict:
+            word_dict[j] += 1
         else:
-            word_dict[k] = 1
-    for j in check_list:
-        check_dict = {}
-        total = 0
-        check_dict = copy.deepcopy(word_dict)
-        if len(j) == word_length:
-            word_check = 0
-            for l in j:
-                if l in check_dict:
-                    check_dict[l] -= 1
-                else:
-                    word_check += 1
-            for v in check_dict.values():
-                if v >= 0:
-                    total += v
-                else:
-                    if v == -1 and word_check == 0:
-                        word_check += 1
+            word_dict[j] = 1
+            
+    for k in word_list[1:]:
+        check = 0
+        check_cnt = 0
+        check_length = len(k)
+        if flag == False:
+            if check_length == word_length:
+                for l in k:
+                    if l in word_dict:
+                        word_dict[l] -= 1
                     else:
-                        total = -10
-                    break
-            if total == 0 or (total == 1 and word_check == 1 ):
-                if word_list[i] not in answer_list:
-                    cnt += 1
-                    answer_list.append(word_list[i])
-                break
-        print(check_dict,word_dict,total)
-print(cnt,check_list,answer_list)
+                        check += 1
 
-# 길이가 같을때 조건을 수정하면 해결될 것 같음.
+                    if check > 1:
+                        break
+                    
+                for l in word_dict.values():
+                    if l > 0 or l < 0:
+                        check_cnt += 1
+
+                if (check == 1 and check_cnt == 1) or (check_cnt == 0 and check == 0):
+                    answer += 1
+            elif check_length + 1 == word_length:
+                for l in k:
+                    if l in word_dict:
+                        word_dict[l] -= 1
+                    else:
+                        check += 1
+
+                    if check > 1:
+                        break
+                    
+                for l in word_dict.values():
+                    if l > 0 or l < 0:
+                        check_cnt += 1
+
+                if (check == 0 and check_cnt == 1):
+                    answer += 1
+            elif check_length - 1 == word_length:
+                for l in k:
+                    if l in word_dict:
+                        word_dict[l] -= 1
+                    else:
+                        check += 1
+
+                    if check > 1:
+                        break
+                    
+                for l in word_dict.values():
+                    if l > 0 or l < 0:
+                        check_cnt += 1
+
+                if (check == 1 and check_cnt == 0):
+                    answer += 1
+        else:
+            if word_length == check_length:
+                for l in k:
+                    if l in word_dict:
+                        word_dict[l] -= 1
+                    else:
+                        check += 1
+
+                    if check >= 1:
+                        break
+                    
+                for l in word_dict.values():
+                    if l < 0 or l > 0:
+                        check_cnt += 1
+
+                if check_cnt == 0 and check == 0:
+                    answer += 1
+        flag = True
+print(answer)
