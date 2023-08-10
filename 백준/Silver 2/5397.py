45bd6d91fa8410e458736c63b3066e8c8b11b3ac
최근영 <ought119@gmail.com>
2023-08-10 17:48:43 +0900

    5397.py 키로거 오늘도 못품

--- 백준/Silver 2/5397.py
+++ 백준/Silver 2/5397.py
@@ -1,5 +1,39 @@
+# import sys
+# from collections import deque
+# 시간초과 코드
+
+# input = sys.stdin.readline
+
+# T = int(input())
+
+# for _ in range(T):
+#     words = input().strip()
+#     ans = deque()
+#     current_pos = 0
+#     for word in words:
+#         if word == "<":
+#             current_pos -= 1
+#             if current_pos < 0:
+#                 current_pos = 0
+#         elif word == ">":
+#             current_pos += 1
+#             if current_pos >= len(ans):
+#                 current_pos = len(ans)
+#         elif word == "-":
+#             if ans != "":
+#                 ans.pop()
+#         else:
+#             if current_pos == 0:
+#                 ans.appendleft(word)
+#                 current_pos += 1
+#             elif current_pos == len(ans):
+#                 ans.append(word)
+#                 current_pos += 1
+#             else:
+                        
+#     print(ans)
+
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
@@ -7,16 +41,28 @@ T = int(input())
 
 for _ in range(T):
     words = input().strip()
-    ans = deque()
+    left_list, right_list = [], []
     last_ope = ""
     for word in words:
         if word == "<":
-            ans.rotate(-1)
+            if left_list:
+                right_list.append(left_list.pop())
+            last_ope = "<"
         elif word == ">":
-            ans.rotate(1)
+            if right_list:
+                left_list.append(right_list.pop())
+            last_ope = ">"
         elif word == "-":
-            if ans != "":
-                ans.pop()
+            if last_ope == ">":
+                if left_list:
+                    left_list.pop()
+            elif last_ope == "<":
+                if right_list:
+                    right_list.pop()
         else:
-            ans.append(word)
-    print(ans)
+            left_list.append(word)
+    
+    while right_list:
+        left_list.append(right_list.pop())
+    
+    print("".join(left_list))
