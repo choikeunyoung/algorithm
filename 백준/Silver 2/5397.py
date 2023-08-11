223bfd9cd69d130acebd9a4ce44ba5e23e954889
최근영 <ought119@gmail.com>
2023-08-11 09:28:54 +0900

    5397.py 문제 풀이

--- 백준/Silver 2/5397.py
+++ 백준/Silver 2/5397.py
@@ -34,34 +34,34 @@
 #     print(ans)
 
 import sys
-
+# 많은 인풋 처리
 input = sys.stdin.readline
 
 T = int(input())
 
 for _ in range(T):
+    # 인풋값 공백 제거 후
     words = input().strip()
+    # 왼쪽 리스트 오른쪽 리스트 생성
     left_list, right_list = [], []
-    last_ope = ""
+    # 반복문을 진행하며
     for word in words:
+        # 만약 < 움직임이면 left 리스트 값이 있으면 right 리스트에 값추가
         if word == "<":
             if left_list:
                 right_list.append(left_list.pop())
-            last_ope = "<"
+        # 만약 > 움직임이면 right 리스트 값이 있으면 left 리스트에 값추가
         elif word == ">":
             if right_list:
                 left_list.append(right_list.pop())
-            last_ope = ">"
+        # 만약 - 이면 left 리스트 값이 있으면 pop
         elif word == "-":
-            if last_ope == ">":
-                if left_list:
-                    left_list.pop()
-            elif last_ope == "<":
-                if right_list:
-                    right_list.pop()
+            if left_list:
+                left_list.pop()
+        # left 리스트에 값을 계속 추가
         else:
             left_list.append(word)
-    
+    # right 리스트 값이 없어질때 까지 뽑아서 왼쪽 리스트에 값추가
     while right_list:
         left_list.append(right_list.pop())
     
