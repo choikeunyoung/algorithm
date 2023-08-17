b3a06f24d8f7737b572a563ecb314f3c880585bf
최근영 <ought119@gmail.com>
2023-08-17 17:32:38 +0900

    codeup 재귀, 백준 재귀문제 푸는중

--- codeup_100/재귀함수/1930.py
+++ codeup_100/재귀함수/1930.py
@@ -1,14 +1,18 @@
 def superSum(start,end):
     global total
-    if start == end:
-        return total + start
+    if start == 0:
+        return total + end
     else:
-        total += start
+        total += DP[start]
         return superSum(start+1, end)
 
 while True:
     try:
         N,M = map(int,input().split())
+        DP = [0] * (M+1)
+        for i in range(M+1):
+            DP[i] = i
+        print(DP)
         total = 0
         print(superSum(N,M))
     except EOFError:
