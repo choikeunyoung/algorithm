c77c8f930b2d24fca62e7315fc93b4a0f051f2b3
최근영 <ought119@gmail.com>
2023-08-17 12:24:10 +0900

    25501.py

---
+++ 백준/Bronze 2/25501.py
@@ -0,0 +1,15 @@
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+T = int(input())
+for i in range(T):
+    word = input()
+    cnt = 0
+    print(isPalindrome(word), cnt)
