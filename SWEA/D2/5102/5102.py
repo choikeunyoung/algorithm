18d7258bcd41ae8a41b0ff59a4fd44640d0e9434
최근영 <ought119@gmail.com>
2023-08-18 11:41:10 +0900

    SWEA BFS 노드 간의 거리

--- SWEA/D2/5102/5102.py
+++ SWEA/D2/5102/5102.py
@@ -1,35 +1,30 @@
-def BFS(start, target):
+def BFS(start,end):
     queue = []
-    for i in range(V):
-        if matrix[start][i] == 1:
-            queue.append(i)
-    visited[start] = True
-    node_cnt[start] = 0
+    queue.append(start)
+    visited[start] = 1
     while queue:
         check = queue.pop(0)
-        visited[check] = True
-        for j in range(V):
-            if matrix[check][j] == 1 and not visited[j]:
-                node_cnt[j] = node_cnt[check] + 1
-                if j == target:
-                    return node_cnt[j]+1
-                else:
-
-                    queue.append(j)
-                    
+        for i in matrix[check]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = visited[check] + 1
+                
+    return visited[end] - 1
+    
 T = int(input())
 
 for tc in range(1, T+1):
     V, E = map(int,input().split())
-    matrix = [[0] * V for _ in range(V)]
-    visited = [False] * V
-    node_cnt = [0] * V
+    matrix = [[] for _ in range(V+1)]
+    visited = [0] * (V+1)
     for _ in range(E):
         A,B = map(int,input().split())
-        matrix[A-1][B-1] = 1
-        matrix[B-1][A-1] = 1
+        matrix[A].append(B)
+        matrix[B].append(A)
+
 
     S,G = map(int,input().split())
-
-    ans = BFS(S-1,G-1)
+    ans = BFS(S,G)
+    if ans < 0:
+        ans = 0
     print(f"#{tc} {ans}")
