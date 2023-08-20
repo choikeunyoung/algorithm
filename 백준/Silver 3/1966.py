51b7df2462ad81f3aa26f4bb96eb391ce10085db
choikeunyoung <ought119@gmail.com>
2023-08-21 01:59:17 +0900

    1966 문제 풀이

--- 백준/Silver 3/1966.py
+++ 백준/Silver 3/1966.py
@@ -6,10 +6,20 @@ input = sys.stdin.readline
 T = int(input())
 
 for tc in range(T):
-    N, search = map(int,input().split())
-    priority_list = list(map(int,input().split()))
-    max_value = max(priority_list)
-    for i,v in enumerate(priority_list):
-        priority_list[i] = (v,i)
+    N, search = map(int, input().split())
+    priority_list = list(map(int, input().split()))
+    for i, v in enumerate(priority_list):
+        priority_list[i] = (v, i)
     priority_list = deque(priority_list)
-    
+    max_value = max(priority_list)[0]
+    cnt = 1
+    while True:
+        if max_value == priority_list[0][0]:
+            if priority_list.popleft()[-1] == search:
+                print(cnt)
+                break
+            else:
+                max_value = max(priority_list)[0]
+            cnt += 1
+        else:
+            priority_list.rotate(-1)
