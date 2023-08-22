0716af59138d3b157206c51c595bf060d7e29131
최근영 <ought119@gmail.com>
2023-08-22 17:43:13 +0900

    농사_로봇.py

--- SWEA/D5/농사 로봇/농사_로봇.py
+++ SWEA/D5/농사 로봇/농사_로봇.py
@@ -1,12 +1,22 @@
 def check_possible(dir):
     check_direction = [0, 0, 0, 0] # 오 위 왼 아
-    for k in range(4):
-        y = dir[0] + direction_list[k]
-        x = dir[1] + direction_list[k]
-        if ( 0 <= x < N ) and ( 0 <= y < N ):
-            if matrix[y][x] != 1 and matrix[y][x] != 2:
-                che
-        
+    current_pos = dir
+    for j in range(1,N+1):
+        for k in range(4):
+            y = dir[0] + direction_list[k]
+            x = dir[1] + direction_list[k]
+            if ( 0 <= x < N ) and ( 0 <= y < N ):
+                if matrix[y][x] != 1 and matrix[y][x] != 2:
+                    if matrix[y][x] == 3:
+                        check_direction[k] = 3
+                    elif matrix[y][x] == 0:
+                        matrix[y][x] = j
+                    else:
+                        check_direction[k] = 2
+        if sum(check_direction) == 0:
+            current_pos[0] += direction_list[0][0]
+            current_pos[1] += direction_list[0][1]
+        elif 
 
 T = int(input())
 # 1 산
