048cdcb6b135028c1a61e431d20a802d9c2ff1d5
최근영 <ought119@gmail.com>
2023-08-24 17:49:02 +0900

    슈퍼마리오.py

---
+++ SWEA/D2/슈퍼마리오/슈퍼마리오.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+num_list = list(map(int,input().split()))
+
+DP = [0] * N
+
+jump = [2, 7]
+
+for i in range(2,N):
+    if i >= 6:
+        middle_check = []
+        for j in jump:
+            DP[]    
+        max(middle_check)
