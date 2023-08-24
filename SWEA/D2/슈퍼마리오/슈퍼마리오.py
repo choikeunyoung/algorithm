2d9bebecce660c1284bee391ef4be9780994c089
choikeunyoung <ought119@gmail.com>
2023-08-25 01:05:05 +0900

    슈퍼마리오.py 푸는중

--- SWEA/D2/슈퍼마리오/슈퍼마리오.py
+++ SWEA/D2/슈퍼마리오/슈퍼마리오.py
@@ -2,13 +2,25 @@ N = int(input())
 
 num_list = list(map(int,input().split()))
 
-DP = [0] * N
+DP = []
 
 jump = [2, 7]
 
-for i in range(2,N):
-    if i >= 6:
-        middle_check = []
-        for j in jump:
-            DP[]    
-        max(middle_check)
+if num_list[1] > num_list[6]:
+    DP.append([num_list[1],1])
+elif num_list[1] < num_list[6]:
+    DP.append([num_list[6],6])
+
+while DP[-1][1] < N:
+    middle_check = []
+    for i in jump:
+        check = DP[-1][1] + i
+        if check < N:
+            middle_check.append([num_list[check],i])
+    if middle_check:
+        if len(middle_check) > 1:
+            if middle_check[0][0] > middle_check[1][0]:
+                DP.append([middle_check[0][0],DP[-1][-1]+2])
+            elif middle_check[0][0] < middle_check[1][0]:
+                DP.append([middle_check[1][0],DP[-1][-1]+7])
+        else:
