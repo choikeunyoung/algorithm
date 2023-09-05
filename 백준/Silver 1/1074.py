bfd6338928e4b4495fe00ff11a30fcc3f52e33b4
choikeunyoung <ought119@gmail.com>
2023-09-06 00:52:54 +0900

    1074.py 내일 풀자

--- 백준/Silver 1/1074.py
+++ 백준/Silver 1/1074.py
@@ -9,25 +9,26 @@ direction = [(-1, -1), (-1, 0), (0, -1), (0, 0)]
 
 def searching(pos, N):
     global cnt
-    if N == 2:
+    if N == 1:
+        print(pos)
         pos[0] -= 1
         pos[1] -= 1
         matrix[pos[0]][pos[1]] = cnt
         cnt += 1
         for dy, dx in direction:
-            matrix[pos[0] + dx][pos[1] + dy] = cnt
+            matrix[pos[0] + dy][pos[1] + dx] = cnt
             cnt += 1
     else:
         for i in range(4):
             if i == 0:
-                searching((pos[0] // 2, pos[1] // 2), N // 2)
+                searching([pos[0] // 2, pos[1] // 2], N // 2)
             elif i == 1:
-                searching((pos[0] // 2, pos[1]), N // 2)
+                searching([pos[0] // 2, pos[1]], N // 2)
             elif i == 2:
-                searching((pos[0], pos[1] // 2), N // 2)
+                searching([pos[0] // 2, pos[1] // 2], N // 2)
             else:
-                searching((pos[0] // 2, pos[1]), N // 2)
+                searching([pos[0] // 2, pos[1]], N // 2)
 
 
-searching(([2**N, 2**N]), N)
+searching(([2**N, 2**N]), 2**N)
 print(matrix)
