904f127a6a1c0eba5cb5debdb3e0df02468fa9e0
최근영 <ought119@gmail.com>
2023-09-12 17:09:26 +0900

    백준/Silver 1/6588.py

--- 백준/Silver 1/6588.py
+++ 백준/Silver 1/6588.py
@@ -1,35 +1,32 @@
 import sys
 
 input = sys.stdin.readline
-
+# 에라토스테네스의 체
 def pri_num():
-    num_list = [True] * 1000000
+    num_list = [False,False] + [True] * 1000000
 
     for i in range(2,round(len(num_list)**0.5)):
         if i%2 == 1:
             if num_list[i] == True:
                 for j in range(2*i, len(num_list), i):
                     num_list[j] = False
-
-    return[ x for x in range(2,len(num_list)) if (num_list[x] and x%2 == 1)]
-
+    return num_list
+# 소수 구해둠
 pri_list = pri_num()
-
-# 모든 소수 리스트를 탐색하는 것이 아닌 방식으로 다른방식으로 
-
+# 반복문을 통해서
 while True:
+    # N값이 0 올때까지 구함
     ans_num = int(input())
-    if ans_num == 0: break
-    flag = 0
-    for k in range(0,len(pri_list)):
-        for l in range(k, len(pri_list)):
-            if ((pri_list[k] + pri_list[l]) == ans_num):
-                flag = 1
+    if ans_num == 0:
+        break
+    # 0부터 시작해서 소수 판별 시작해서 차가 가장 큰 경우는 값이 작을때
+    for i in range(2,ans_num+1):
+        # 홀수인 경우
+        if i % 2 == 1:
+            # ans_num 에서 i 뺀값과 i 값이 소수인 경우 값 출력 후 탈출
+            if pri_list[ans_num - i] and pri_list[i]:
+                print(ans_num,"=",i,"+",(ans_num-i))
                 break
-            elif ((pri_list[k] + pri_list[l] > ans_num)):
-                break
-        if flag == 1:
-            print(ans_num,"=",pri_list[k],"+",pri_list[l])
-            break
+    # 모든 경우가 안맞으면 출력
     else:
         print("Goldbach's conjecture is wrong.")
