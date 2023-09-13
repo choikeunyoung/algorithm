4a5b26202fbf725469049b70d73a97ac4b56a558
최근영 <ought119@gmail.com>
2023-09-13 18:02:46 +0900

    2504.py

---
+++ 백준/Gold 5/2504.py
@@ -0,0 +1,16 @@
+info = input().strip()
+stack = []
+check = ""
+flag = 0
+number = 0
+word = input()
+
+stack = []
+
+for i in word:
+    if stack:
+    
+    else:
+        if i == ")" or i == "]":
+            print(0)
+            break
