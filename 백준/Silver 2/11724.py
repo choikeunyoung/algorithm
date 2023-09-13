84732813b00bcd4212a562d26f37061f1ba0a305
choikeunyoung <ought119@gmail.com>
2023-09-14 00:56:15 +0900

    11724 주석 추가 문제풀이

--- 백준/Silver 2/11724.py
+++ 백준/Silver 2/11724.py
@@ -1,17 +1,36 @@
-N = int(input())
-check_list = []
-for _ in range(N):
-    check_list.append(list(map(str,input())))
-for i in range(len(check_list[0])):
-    check_str = ""
-    cnt = 0
-    for j in range(len(check_list)):
-        if check_str == "":
-            check_str = check_list[j][i]
-        else:
-            if check_str == check_list[j][i]:
-                cnt += 1
-    if cnt == N-1:
-        print(check_list[j][i], end="")
-    else:
-        print("?", end="")
+import sys
+
+input = sys.stdin.readline
+
+
+# DFS 시행
+def DFS(start):
+    visited[start] = True
+    stack = graph[start]
+    while stack:
+        check = stack.pop()
+        if not visited[check]:
+            visited[check] = True
+            stack.extend(graph[check])
+
+
+# 노드와 간선의 갯수 입력
+N, M = map(int, input().split())
+# graph 형태로 생성
+graph = [[] for _ in range(N + 1)]
+# 방문처리 체크 리스트
+visited = [False] * (N + 1)
+for _ in range(M):
+    # A, B 를 받아옴
+    A, B = map(int, input().split())
+    # 방향이 없으므로 양쪽에 추가
+    graph[A].append(B)
+    graph[B].append(A)
+
+cnt = 0
+# N+1 까지 반복을 시행하며 DFS 를 시행 한 횟수 체크
+for i in range(1, N + 1):
+    if not visited[i]:
+        DFS(i)
+        cnt += 1
+print(cnt)
