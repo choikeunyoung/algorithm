6c985c9f265011e0169cd58b8887564c62bd2a44
choikeunyoung <ought119@gmail.com>
2023-09-21 01:32:04 +0900

    치킨 배달 푸는중

--- 백준/Gold 5/15686.py
+++ 백준/Gold 5/15686.py
@@ -2,44 +2,49 @@ def check_value(first_list, second_list):
     ans = 0
     for i in first_list:
         for j in second_list:
-            visited[j[0]][j[1]] = min(visited[j[0]][j[1]],abs(i[0]-j[0])+abs(i[1]-j[1]))
+            visited[j[0]][j[1]] = min(
+                visited[j[0]][j[1]], abs(i[0] - j[0]) + abs(i[1] - j[1])
+            )
+
+
 one_pos = []
 two_pos = []
 
-N, M = map(int,input().split())
+N, M = map(int, input().split())
 
-matrix = [ list(map(int,input().split())) for _ in range(N) ]
-visited = [ [10**6] * N for _ in range(N) ]
+matrix = [list(map(int, input().split())) for _ in range(N)]
+visited = [[10**6] * N for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if matrix[i][j] == 1:
-            one_pos.append((i,j))
+            one_pos.append((i, j))
         elif matrix[i][j] == 2:
-            two_pos.append((i,j))
+            two_pos.append((i, j))
 
 min_value = 10**6
-check = []
+check = set()
 
 for i in two_pos:
     total = 0
     for j in one_pos:
-        total += (abs(i[0]-j[0])+abs(i[1]-j[1]))
-    check.append(total)
-    
-if len(check) == M:
-    check_value(two_pos,one_pos)
-else:
-    ans = []
-    while M > 0:
-        min_check = min(check)
-        for i in range(len(check)):
-            if check[i] == min_check:
-                check.pop(i)
-                ans.append(two_pos[i])
-                break
-        M -= 1    
-    print(ans, two_pos, check)
-    check_value(ans,one_pos)
+        total += abs(i[0] - j[0]) + abs(i[1] - j[1])
+    check.add(total)
+
+check_value(two_pos, one_pos)
+# if len(check) <= M:
+# else:
+#     check = list(check)
+#     ans = []
+#     while M > 0:
+#         min_check = min(check)
+#         for i in range(len(check)):
+#             if check[i] == min_check:
+#                 check.pop(i)
+#                 ans.append(two_pos[i])
+#                 break
+#         M -= 1
+#     print(ans, two_pos, check)
+#     check_value(ans, one_pos)
 answer = 0
 for i in range(N):
     for j in range(N):
