64240147ac0714c92dca3342d5ebe8dedfeabff5
최근영 <ought119@gmail.com>
2023-09-20 17:57:48 +0900

    치킨 배달 푸는중

--- 백준/Gold 5/15686.py
+++ 백준/Gold 5/15686.py
@@ -1,12 +1,15 @@
-from collections import deque
-
+def check_value(first_list, second_list):
+    ans = 0
+    for i in first_list:
+        for j in second_list:
+            visited[j[0]][j[1]] = min(visited[j[0]][j[1]],abs(i[0]-j[0])+abs(i[1]-j[1]))
 one_pos = []
 two_pos = []
 
 N, M = map(int,input().split())
 
 matrix = [ list(map(int,input().split())) for _ in range(N) ]
-
+visited = [ [10**6] * N for _ in range(N) ]
 for i in range(N):
     for j in range(N):
         if matrix[i][j] == 1:
@@ -15,11 +18,31 @@ for i in range(N):
             two_pos.append((i,j))
 
 min_value = 10**6
-print(two_pos, one_pos)
-for i in one_pos:
-    total = 10**6
-    for j in two_pos:
-        total = min(total,((i[0]-j[0])**2+(i[1]-j[1])**2))
-    if min_value > total:
-        min_value = total
-print(min_value)
+check = []
+
+for i in two_pos:
+    total = 0
+    for j in one_pos:
+        total += (abs(i[0]-j[0])+abs(i[1]-j[1]))
+    check.append(total)
+    
+if len(check) == M:
+    check_value(two_pos,one_pos)
+else:
+    ans = []
+    while M > 0:
+        min_check = min(check)
+        for i in range(len(check)):
+            if check[i] == min_check:
+                check.pop(i)
+                ans.append(two_pos[i])
+                break
+        M -= 1    
+    print(ans, two_pos, check)
+    check_value(ans,one_pos)
+answer = 0
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] != 10**6:
+            answer += visited[i][j]
+print(answer)
