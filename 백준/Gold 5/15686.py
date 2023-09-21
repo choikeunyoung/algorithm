4067ba9baf43b8b1eb56d07903dee90958ad6dfa
최근영 <ought119@gmail.com>
2023-09-21 17:52:56 +0900

    치킨 배달 풀음

--- 백준/Gold 5/15686.py
+++ 백준/Gold 5/15686.py
@@ -1,11 +1,9 @@
+from itertools import combinations
+
 def check_value(first_list, second_list):
-    ans = 0
     for i in first_list:
         for j in second_list:
-            visited[j[0]][j[1]] = min(
-                visited[j[0]][j[1]], abs(i[0] - j[0]) + abs(i[1] - j[1])
-            )
-
+            visited[j[0]][j[1]] = min(visited[j[0]][j[1]], abs(i[0] - j[0]) + abs(i[1] - j[1]))
 
 one_pos = []
 two_pos = []
@@ -13,7 +11,6 @@ two_pos = []
 N, M = map(int, input().split())
 
 matrix = [list(map(int, input().split())) for _ in range(N)]
-visited = [[10**6] * N for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if matrix[i][j] == 1:
@@ -22,32 +19,17 @@ for i in range(N):
             two_pos.append((i, j))
 
 min_value = 10**6
-check = set()
-
+two_pos = list(combinations(two_pos,M))
+ans = 10**6
 for i in two_pos:
-    total = 0
-    for j in one_pos:
-        total += abs(i[0] - j[0]) + abs(i[1] - j[1])
-    check.add(total)
+    visited = [[10**6] * N for _ in range(N)]
+    check_value(i, one_pos)
+    answer = 0
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] != 10**6:
+                answer += visited[i][j]
+    if answer < ans:
+        ans = answer
 
-check_value(two_pos, one_pos)
-# if len(check) <= M:
-# else:
-#     check = list(check)
-#     ans = []
-#     while M > 0:
-#         min_check = min(check)
-#         for i in range(len(check)):
-#             if check[i] == min_check:
-#                 check.pop(i)
-#                 ans.append(two_pos[i])
-#                 break
-#         M -= 1
-#     print(ans, two_pos, check)
-#     check_value(ans, one_pos)
-answer = 0
-for i in range(N):
-    for j in range(N):
-        if visited[i][j] != 10**6:
-            answer += visited[i][j]
-print(answer)
+print(ans)
