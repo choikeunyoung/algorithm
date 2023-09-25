827db9f13ec6287ecf839d77a8521a3397080b04
choikeunyoung <ought119@gmail.com>
2023-09-25 20:26:09 +0900

    마법의_물뿌리개.py

--- SWEA/D5/마법의 물뿌리개/마법의_물뿌리개.py
+++ SWEA/D5/마법의 물뿌리개/마법의_물뿌리개.py
@@ -9,25 +9,22 @@ for tc in range(1, T + 1):
     for i in range(N):
         tree_list[i] = max_tree - tree_list[i]
         total += tree_list[i]
-    
+    print(total)
     if total == 0:
         print(f"#{tc} {day}")
     else:
-        for i in range(N):
-            if tree_list[i]//4 > 0 and tree_list[i] != 0:
-                day += (tree_list[i]//4*3)
-                tree_list[i] = tree_list[i] - (tree_list[i]//4)*4
-                if tree_list[i]%4 == 2 or tree_list[i] == 3:
-                    day += 2
-                elif tree_list[i]%2 == 1:
-                    day += 1
-                else:
-                    day += 3
-            elif tree_list[i]//4 == 0 and tree_list[i] != 0:
-                if tree_list[i]%4 == 2 or tree_list[i] == 3:
-                    day += 2
-                elif tree_list[i]%2 == 1:
-                    day += 1
-                else:
-                    day += 3
+        while total > 0:
+            day += 1
+            if day % 2 == 0:
+                for k in range(N):
+                    if tree_list[k] != 0 and tree_list[k]%2 == 0:
+                        tree_list[k] -= 2
+                        total -= 2
+                        break
+            else:
+                for k in range(N):
+                    if tree_list[k] != 0 and tree_list[k]%2 == 1:
+                        tree_list[k] -= 1
+                        total -= 1
+                        break
         print(f"#{tc} {day}")
