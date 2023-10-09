c9fe5f86a50b396a4cda0062633de9fe3e27dc7c
choikeunyoung <ought119@gmail.com>
2023-10-09 18:13:37 +0900

    17425.py 주석추가 문제풀이

--- 백준/Gold 4/17425.py
+++ 백준/Gold 4/17425.py
@@ -1,22 +1,27 @@
 import sys
+# 약수 구하는 함수
+def divisor():
+    # 약수를 저장할 리스트
+    num_list = [1] * 1000001
+    # 정답을 출력할 리스트
+    ans = [0] * 1000001
+    # 2부터 1,000,000 까지 반복
+    for i in range(2,1000001):
+        # i 부터 1,000,000 까지 반복
+        for j in range(i ,1000001,i):
+            # 각 1씩 저장된 리스트 j 에 i 값을 더해줌
+            # i 값을 더하는 이유가 i 의 배수이기 때문에
+            num_list[j] += i
+    # 저장된 num_list의 i 값과 ans 의 이전값을 더해나감
+    for i in range(1,1000001):
+        ans[i] += ans[i-1] + num_list[i]
 
-# 미리 연산으로 약수와 약수의 합을 가져와서 연산한다 => 메모리 사용이 많기 때문에 풀때 메모리를 많이 사용하는지 확인해보기
+    return ans
 
-def test():
-    num_dict = {}
-    for i in range(1,10000001):
-        j = 0
-        for k in range(1,i+1):
-            j += (k*(i//k))
-        print(j)
-    return num_dict
+input = sys.stdin.readline
+answer = divisor()
+T = int(input())
 
-print(test())
-
-# stdin = sys.stdin.readline
-
-# T = int(stdin())
-
-# for _ in range(T):
-#     N = int(stdin())
-#     print(test(N))
+for _ in range(T):
+    num = int(input())
+    print(answer[num])
