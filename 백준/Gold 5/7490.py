51495511acfb3bcdae8d9baef685b58210f3ded5
최근영 <ought119@gmail.com>
2023-10-10 11:57:06 +0900

    7490.py 주석 추가

--- 백준/Gold 5/7490.py
+++ 백준/Gold 5/7490.py
@@ -1,22 +1,32 @@
+# 연산 결과가 0인 값을 출력할 함수
 def calculator(num):
     global ans
+    # 연산을 진행할 변수
     answer = ""
+    # 정답을 출력할 변수
+    temp = ""
+    # N값과 num이 같으면
     if num == N:
+        # 마지막 값을 리스트에 추가
         ans.append(str(num))
+        # ans 값을 순회하며
         for i in ans:
+            # 공백인 경우 정답을 출력할 변수에 값을 더해줌
             if i == " ":
-                if eval(answer) != 0:
-                    answer = str(eval(answer))
-                else:
-                    answer = answer + i
+                temp += i
+            # 외의 경우 둘다 값 추가
             else:
                 answer += i
+                temp += i
+        # 연산한 결과가 0인 경우 temp 출력
         if eval(answer) == 0:
-            print(answer)
+            print(temp)
+        # 백트래킹
         ans.pop()
         return
     else:
-        for i in ["+", "-", " "]:
+        # 연산자를 반복하며 재귀와 백트래킹
+        for i in [" ","+", "-"]:
             ans.append(str(num))
             ans.append(i)
             calculator(num+1)
@@ -27,5 +37,5 @@ T = int(input())
 for _ in range(T):
     N = int(input())
     ans = []
-    
     calculator(1)
+    print()
