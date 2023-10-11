cf6e6b57952272ac1f7ef0589e9d81f3d6f9ac21
choikeunyoung <ought119@gmail.com>
2023-10-11 20:42:51 +0900

    1662.py 거의 다 품

--- 백준/Gold 5/1662.py
+++ 백준/Gold 5/1662.py
@@ -1,9 +1,25 @@
-def find_word(num):
-    if num == word_length:
-        return
-    else:
-        
-
-word = input()
+word = input().strip()
 word_length = len(word)
-print(len(answer))
+answer = []
+ans = ""
+
+for i in word:
+    if i == "(":
+        answer.append(ans)
+        ans = ""
+    elif i == ")":
+        if ans != "":
+            check = int(answer.pop())
+            rest = check%10
+            ans *= rest
+            check //= 10
+            if check != 0:
+                check = str(check)
+                ans = check + ans
+    else:
+        ans += i
+print(ans)
+if ans == "":
+    print(0)
+else:
+    print(len(ans))
