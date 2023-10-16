e9890dd2345e3664f5161ebcbdeaab602e6bb47f
choikeunyoung <ought119@gmail.com>
2023-10-17 01:38:01 +0900

    1913.py 달패잉 푸는중

--- 백준/Silver 3/1913.py
+++ 백준/Silver 3/1913.py
@@ -1,7 +1,22 @@
 N = int(input())
 search = int(input())
 
-col = 0
-row = 0
-
-matrix = [ i for i in range(1,N*N+1)]
+start = N*N
+direction = [1, 1, -1, -1]
+matrix = [ [0] * N for _ in range(N)]
+pos = [0,0]
+index = 0
+while start != 0:
+    matrix[pos[0]][pos[1]] = start
+    if index % 2 == 0:
+        if 0 <= pos[0] < N-1:
+            pos[0] += direction[index]
+        else:
+            index = (index+1)%4
+    else:
+        if 0 <= pos[1] < N-1:
+            pos[1] += direction[index]
+        else:
+            index = (index+1)%4
+    start -= 1
+    print(matrix)
