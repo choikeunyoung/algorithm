b5a93dad0f0903c2c152f61cbbc0b148a3513289
choikeunyoung <ought119@gmail.com>
2023-10-20 01:43:12 +0900

    1245.py 농장 관리

--- 백준/Gold 5/1245.py
+++ 백준/Gold 5/1245.py
@@ -3,30 +3,21 @@ from pprint import pprint
 
 direction = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
 def BFS(start):
-    global cnt
+    global check_tree
     q = deque([start])
-    visited[start[0]][start[1]] = matrix[start[0]][start[1]]
+    visited[start[0]][start[1]] = True
     while q:
-        flag = 0
         check = q.popleft()
         for k in range(8):
             ny = check[0] + direction[k][0]
             nx = check[1] + direction[k][1]
             if ( 0 <= ny < N ) and ( 0 <= nx < M ):
-                if matrix[ny][nx] > matrix[check[0]][check[1]] and visited[check[0]][check[1]] >= matrix[ny][nx]:
-                    flag = 1
-                    visited[ny][nx] = visited[check[0]][check[1]]
-                    q = deque([])
-                    break
-                elif matrix[ny][nx] == matrix[start[0]][start[1]] and not visited[ny][nx]:
-                    visited[ny][nx] = matrix[start[0]][start[1]]
+                if matrix[ny][nx] > matrix[check[0]][check[1]]:
+                    check_tree = False
+                
+                if not visited[ny][nx] and matrix[ny][nx] == matrix[check[0]][check[1]]:
+                    visited[ny][nx] = True
                     q.append((ny,nx))
-                elif matrix[ny][nx] < matrix[check[0]][check[1]] or visited[check[0]][check[1]] < matrix[ny][nx]:
-                    visited[check[0]][check[1]] = visited[ny][nx]
-        if flag == 1:
-            break
-    else:
-        cnt += 1
 
 
 
@@ -39,8 +30,9 @@ visited = [ [0] * M for _ in range(N) ]
 cnt = 0
 for i in range(N):
     for j in range(M):
-        if matrix[i][j] != 0:
+        if not visited[i][j]:
+            check_tree = True
             BFS((i,j))
-            pprint(visited)
-            print(i,j,cnt)
+            if check_tree:
+                cnt += 1
 print(cnt)
