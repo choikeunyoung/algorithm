0c29f625008eb2a635b4c12f39188905c8c83feb
choikeunyoung <ought119@gmail.com>
2023-11-07 01:18:06 +0900

    2468.py

--- 백준/Silver 1/2468.py
+++ 백준/Silver 1/2468.py
@@ -1,3 +1,15 @@
+from collections import deque
+
+direction = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
 N = int(input())
 
 matrix = [list(map(int, input().split())) for _ in range(N)]
+
+use = []
+visited = [[False] * N for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            BFS((i, j))
