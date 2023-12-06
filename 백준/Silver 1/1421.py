e092a4fce78168f91ac5d610f10c2d18c2d6e452
choikeunyoung <ought119@gmail.com>
2023-12-07 04:47:32 +0900

    1421.py 주석추가

--- 백준/Silver 1/1421.py
+++ 백준/Silver 1/1421.py
@@ -3,33 +3,41 @@ import sys
 input = sys.stdin.readline
 
 N, C, W = map(int, input().split())
-
+# 나무를 담을 리스트
 wood_list = []
 
 for _ in range(N):
     wood_list.append(int(input()))
-
+# 나무를 작은값부터 큰수로 오름차순 정렬
 wood_list.sort()
 
 max_wood = 0
-
+# 1부터 최대 나무길이까지 반복
 for i in range(1, wood_list[-1] + 1):
+    # 현재 가격변수 선언
     current_value = 0
-    cut_cnt = 0
+    # 나무 리스트를 반복하며
     for j in wood_list:
+        # 자르는 길이가 나무보다 작은경우
         if j > i:
-            cut_cnt += j // i
-            if j % i == 0:
+            # 나무 자른 횟수체크
+            cut_cnt = j // i
+            # 길이가 남았는지 확인할 변수
+            r = j % i
+            # 나머지가 없으면 자르는 횟수 -1
+            if r == 0:
                 cut_cnt -= 1
-                current_value += (j // i - 1) * i
-            else:
-                current_value += (j // i) * i
+            # 이득값 계산해는 식 몫 * W * 현재 나무 길이 - 자른 횟수 * 자르는 비용
+            profit = ((j // i) * W * i) - (cut_cnt * C)
+            # 이득값이 음수면 패스
+            if profit < 0:
+                continue
+            # profit 을 현재 값에 더해나감
+            current_value += profit
+        # 그냥 j와 i가 같으면 자를필요없이 값 더해줌
         elif j == i:
-            current_value += i
-    current_value -= cut_cnt * C
+            current_value += i * W
+    # 최대값 비교
     if current_value > max_wood:
-        print(current_value, i, cut_cnt)
         max_wood = current_value
 print(max_wood)
-
-# 자르는 비용이 파는 비용보다 큰 경우 고려해야함
