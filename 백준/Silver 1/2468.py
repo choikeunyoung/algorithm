c233034865a14abf431cc281851009dbe2ccf9ee
choikeunyoung <ought119@gmail.com>
2024-01-11 02:55:35 +0900

    2468.py

--- 백준/Silver 1/2468.py
+++ 백준/Silver 1/2468.py
@@ -5,11 +5,10 @@ direction = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1
 N = int(input())
 
 matrix = [list(map(int, input().split())) for _ in range(N)]
-
+visited_dict = {}
 use = []
 visited = [[False] * N for _ in range(N)]
 
 for i in range(N):
     for j in range(N):
-        if not visited[i][j]:
-            BFS((i, j))
+        visited_dict[matrix[i][j]] = 1
