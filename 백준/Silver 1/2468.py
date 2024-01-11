beaaeffa629ae67786f4996f70f32b6e05193302
choikeunyoung <ought119@gmail.com>
2024-01-12 02:16:04 +0900

    2468.py 푸는중

--- 백준/Silver 1/2468.py
+++ 백준/Silver 1/2468.py
@@ -7,8 +7,18 @@ N = int(input())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 visited_dict = {}
 use = []
-visited = [[False] * N for _ in range(N)]
 
 for i in range(N):
     for j in range(N):
         visited_dict[matrix[i][j]] = 1
+
+total_length = len(visited_dict.keys())
+
+for i in visited_dict.keys():
+    visited = [[False] * N for _ in range(N)]
+    for k in range(N):
+        for l in range(N):
+            if matrix[k][l] <= i:
+                visited[k][l] = True
+    
+    for k in range(N):
