d4757902e9f4fb4bf3a13f5ec0520973ea13830e
김현지 <hanguel97@gmail.com>
2024-01-14 00:40:30 +0900

    1706.py 주석 추가

--- 백준/Silver 2/1706.py
+++ 백준/Silver 2/1706.py
@@ -1,46 +1,37 @@
 R, C = map(int,input().split())
 
-
 matrix = [ input() for _ in range(R) ]
 
 word_list = []
-
-for i in matrix:
-    middle_list = i.split("#")
-    if len(middle_list) > 1:
-        max_cnt = 0
-        check = ""
-        for j in middle_list:
-            if j != "":
-                if max_cnt < len(j):
-                    max_cnt = len(j)
-                    word = j
-        if max_cnt > 1:
-            word_list.append(word)
-    else:
-        if len(i) > 1:
-            word_list.append(i)
-
+# 가로 방향으로 반복하며 # 을 제외한 문자들을 정답 리스트에 저장
+for i in range(R):
+    word = ""
+    for j in range(C):
+        if matrix[i][j] == "#":
+            # 막힌 부분에서 빈문자가 아니고 문자 길이가 1이 아닌 경우 추가 후 word 값 초기화
+            if word != "":
+                if len(word) > 1:
+                    word_list.append(word)
+            word = ""
+        else:
+            word += matrix[i][j]
+    if word != "" and len(word) > 1:
+        word_list.append(word)
+# 세로 방향으로 반복하며 # 을 제외한 문자들을 리스트에 저장
 for i in range(C):
     word = ""
-    max_cnt = 0
-    check = ""
     for j in range(R):
-        if matrix[j][i] != "#":
-            word += matrix[j][i]
-        elif matrix[j][i] == "#":
-            if max_cnt < len(word):
-                max_cnt = len(word)
-                check = word
+        if matrix[j][i] == "#":
+            if word != "":
+                if len(word) > 1:
+                    word_list.append(word)
             word = ""
-    if word != "":
-        if max_cnt < len(word):
-            max_cnt = len(word)
-            check = word
-    if max_cnt != 1:
-        word_list.append(check)
+        else:
+            word += matrix[j][i]
+    if word != "" and len(word) > 1:
+        word_list.append(word)
 
-for k in sorted(word_list):
-    if k != "":
-        print(k)
-        break
+word_list.sort()
+
+if word_list:
+    print(word_list[0])
