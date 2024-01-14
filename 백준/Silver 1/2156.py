71e25a16a113d1b8fe9dd59fad429ea7deba0940
choikeunyoung <ought119@gmail.com>
2024-01-15 01:41:28 +0900

    2156.py 푸는중

--- 백준/Silver 1/2156.py
+++ 백준/Silver 1/2156.py
@@ -5,11 +5,17 @@ input = sys.stdin.readline
 N = int(input())
 
 beer = []
-
 for _ in range(N):
     beer.append(int(input()))
+if N == 1:
+    print(beer[0])
+elif N == 2:
+    print(beer[1] + beer[0])
+else:
+    dp = [beer[0], beer[1] + beer[0]]
+    dp.append(max(beer[1] + beer[2], beer[0] + beer[2]))
 
-dp = [[(beer[0],1),(0,0)]]
+    for i in range(3, N):
+        dp.append(max(beer[i] + beer[i - 1] + dp[i - 3], beer[i] + dp[i - 2]))
 
-for i in range(1,N):
-    
+    print(max(dp))
