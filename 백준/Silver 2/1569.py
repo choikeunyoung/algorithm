b8896af97eba71fe0554a45f58e02c494b86c25b
최근영 <ought119@gmail.com>
2024-01-31 21:38:58 +0900

    1569.py 거의 다 품

--- 백준/Silver 2/1569.py
+++ 백준/Silver 2/1569.py
@@ -28,19 +28,29 @@ cnt = 0
 
 if y_total > x_total:    
     for k in check_list:
-        if k[0]
+        if (min_x <= k[0] <= max_x + (y_total - x_total) and ( min_y == k[1] or max_y == k[1])) or (min_x - (y_total - x_total) <= k[0] < min_x and  (min_y == k[1] or max_y == k[1])):
+            cnt += 1
 
+elif y_total < x_total:
+    for k in check_list:
+        if (( min_x == k[0] or max_x == k[0] ) and min_y <= k[1] <= max_y + (x_total - y_total)) or (( min_x == k[0] or max_x == k[0] ) and min_y - (x_total - y_total) <= k[1] < min_y ):
+            cnt += 1
 else:
-    for i in range(min_y, max_y+1 + (x_total - y_total)):
-        square_point.add((min_x,i))
-        square_point.add((max_x,i))
+    for k in check_list:
+        if (( min_x == k[0] or max_x == k[0]) and (min_y <= k[1] <= max_y)) or ((min_x <= k[0] <= max_x) or (min_y == k[1] or max_y == k[1])):
+            cnt += 1
+            print(k)
+        print(k, cnt, min_x, min_y, max_x, max_y)
 
+    # for i in range(min_y, max_y+1 + (x_total - y_total)):
+    #     square_point.add((min_x,i))
+    #     square_point.add((max_x,i))
 
-    for i in range(min_x+1, max_x):
-        square_point.add((i,min_y))
-        square_point.add((i,max_y + (x_total - y_total)))
+    # for i in range(min_x+1, max_x):
+    #     square_point.add((i,min_y))
+    #     square_point.add((i,max_y + (x_total - y_total)))
 
-
+print(cnt)
 if cnt == N:
     print(max(y_total,x_total))
 else:
