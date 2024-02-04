1c8e025b767f5a4f4754c8071e6af9b5b1aeaf42
최근영 <ought119@gmail.com>
2024-02-04 21:29:09 +0900

    거의 다품

--- 백준/Silver 2/1569.py
+++ 백준/Silver 2/1569.py
@@ -26,31 +26,25 @@ for _ in range(N):
 
 cnt = 0
 
-if y_total > x_total:    
+if y_total > x_total:
+    max_distance = y_total - x_total
     for k in check_list:
-        if (min_x <= k[0] <= max_x + (y_total - x_total) and ( min_y == k[1] or max_y == k[1])) or (min_x - (y_total - x_total) <= k[0] < min_x and  (min_y == k[1] or max_y == k[1])):
+        if (min_x <= k[0] <= max_x + max_distance and ( min_y == k[1] or max_y == k[1] + max_distance)) or (min_x - max_distance <= k[0] < min_x and  (min_y == k[1] or max_y == k[1] + max_distance)):
             cnt += 1
 
 elif y_total < x_total:
+    max_distance = x_total - y_total
     for k in check_list:
         if (( min_x == k[0] or max_x == k[0] ) and min_y <= k[1] <= max_y + (x_total - y_total)) or (( min_x == k[0] or max_x == k[0] ) and min_y - (x_total - y_total) <= k[1] < min_y ):
             cnt += 1
+
 else:
     for k in check_list:
         if (( min_x == k[0] or max_x == k[0]) and (min_y <= k[1] <= max_y)) or ((min_x <= k[0] <= max_x) or (min_y == k[1] or max_y == k[1])):
             cnt += 1
             print(k)
-        print(k, cnt, min_x, min_y, max_x, max_y)
 
-    # for i in range(min_y, max_y+1 + (x_total - y_total)):
-    #     square_point.add((min_x,i))
-    #     square_point.add((max_x,i))
 
-    # for i in range(min_x+1, max_x):
-    #     square_point.add((i,min_y))
-    #     square_point.add((i,max_y + (x_total - y_total)))
-
-print(cnt)
 if cnt == N:
     print(max(y_total,x_total))
 else:
