f4003f3eade81f8f2de22afe226437ded324c1f7
choikeunyoung <ought119@gmail.com>
2024-03-03 05:57:15 +0900

    거의 다품 1753

--- 백준/Gold 4/1753.py
+++ 백준/Gold 4/1753.py
@@ -1,8 +1,31 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
+
+input = sys.stdin.readline
+
+def BFS(init):
+    queue = deque(matrix[init])
+    visited[init] = True
+    for i in matrix[init]:
+        distance[i[0]] = i[1]
+
+    while queue:
+        now, dist = queue.popleft()
+
+        if distance[now]<dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in matrix[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]]=cost
+                queue.append((i[0],cost))
 
 V, E = map(int,input().split())
 
+visited = [False] * (V+1)
+distance = [10**9] * (V+1)
 start = int(input())
 
 matrix = defaultdict(list)
@@ -11,14 +34,13 @@ for _ in range(E):
     S, E, W = map(int,input().split())
     matrix[S].append((E, W))
 
-visited = [False] * (V+1)
-distance = [10**9] * (V+1)
+BFS(start)
 
-for i in matrix[start]:
-    distance[i[0]] = i[1]
-
-
-
-for i in range(V-1):
-    current = 0
-    for k in range(1, V+1):
+for i in range(1,V+1):
+    if i != start:
+        if distance[i] == 10**9:
+            print("INF")
+        else:
+            print(distance[i])
+    else:
+        print(0)
