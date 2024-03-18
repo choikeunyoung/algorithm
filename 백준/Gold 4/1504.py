7a0038647471edf6b71df6944a77dd5ffeb5b54c
최근영 <ought119@gmail.com>
2024-03-18 20:54:26 +0900

    1504 푸는 중

--- 백준/Gold 4/1504.py
+++ 백준/Gold 4/1504.py
@@ -5,13 +5,24 @@ input = sys.stdin.readline
 
 N,M = map(int,input().split())
 
-matrix = [ [] for _ in range(N)]
+matrix = [ [] for _ in range(N+1)]
+distance = [10 ** 9] * (N+1)
 
 for _ in range(M):
     S, E, W = map(int,input().split())
     matrix[S].append((W,E))
     matrix[E].append((W,S))
 
-target
+target_1, target_2 = map(int,input().split())
 
-def dijkstra():
+
+
+def dijkstra(start, end):
+    queue = []
+    heapq.heappush(queue, matrix[start])
+    while queue:
+        dist, now = heapq.heappop(queue)
+        if now == end:
+            
+            return 
+        if distance[now] == 10 ** 9:
