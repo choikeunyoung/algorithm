e3e4cc6364cf788043db03942d4a3c41804aa1f2
최근영 <ought119@gmail.com>
2024-04-17 21:05:56 +0900

    7682.py 주석추가 문제풀이

--- 백준/Gold 5/7682.py
+++ 백준/Gold 5/7682.py
@@ -20,6 +20,7 @@ while True:
                 circle_flag = 0
                 cross_check = 0
                 circle_check = 0
+                # 대각 선 범위 체크
                 if i == 0:
                     if matrix[i][0] == "X":
                         if matrix[i+1][1] == "X":
@@ -61,7 +62,7 @@ while True:
                         if matrix[i+1][2] == "O":
                             if matrix[i+2][2] == "O":
                                 circle_flag = 1
-
+                # 가로 개수 체크
                 for j in range(3):
                     circle_check = matrix[j].count("O")
                     cross_check = matrix[j].count("X")
@@ -70,9 +71,10 @@ while True:
 
                     if cross_check == 3:
                         cross_flag = 1
-                
+                # X 빙고일 경우 O 빙고가 있는지 체크
                 if cross_flag == 1:
                     if circle_flag != 1:
+                        # X 빙고의 경우 O의 개수가 작아야 하는거 체크
                         if cross > circle:
                             print("valid")
                         else:
@@ -80,12 +82,14 @@ while True:
                     else:
                         print("invalid")
                     break
-                
+                # O 빙고의 경우 X 빙고가 있는지 체크
                 if circle_flag == 1:
                     if cross_flag != 1:
+                        # O 빙고의 경우 꽉차있는 경우 나올 수 없으므로 체크
                         if dot == 0:
                             print("invalid")
                         else:
+                            #  꽉 차있지 않는 경우 X와 O의 개수가 같아야 하기 떄문에 체크
                             if cross == circle:
                                 print("valid")
                             else:
@@ -94,6 +98,7 @@ while True:
                         print("invalid")
                     break
             else:
+                # 모든 경우를 통과했을때 꽉 차있는지 체크
                 if dot == 0:
                     if cross == 5 and circle == 4:
                         print("valid")
