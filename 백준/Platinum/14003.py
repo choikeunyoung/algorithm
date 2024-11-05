f72b1cf00ef7493a8fccaa07ab9184ec5edb9b28
최근영 <ought119@gmail.com>
2024-11-05 16:21:47 +0900

    가장 긴증가수열푸는중

--- 백준/Platinum/14003.py
+++ 백준/Platinum/14003.py
@@ -4,7 +4,7 @@ def binary_search(right,value):
     middle = 0
     while left < right:
         middle = (left + right) // 2
-        if dp[middle] < value:
+        if dp[middle][0] < value:
             left = middle + 1
         else:
             right = middle
@@ -16,33 +16,25 @@ num_list = list(map(int,input().split()))
 # DP 리스트 길이 1
 dp_length = 1
 # 초기값 0번 값
-dp = [num_list[0]]
+dp = [(num_list[0],0)]
 # 각 숫자 인덱스별 길이가 몇인지 체크
-dp_array = [0]
+path = [-1] * N
 # 반복문 1 ~ N 까지
 for i in range(1,N):
     # 끝의 값이 들어온 num_list[i] 값보다 작은 경우
-    if dp[-1] < num_list[i]:
+    if dp[-1][0] < num_list[i]:
         # dp 길이 체크 변수 1 증가
         dp_length += 1
         # dp 에 들어온 값 추가
-        dp.append(num_list[i])
+        dp.append((num_list[i],dp[-1][1]))
         # 각 인덱스 별 최대 길이 추가
-        dp_array.append(dp_length - 1)
+        path[i] = dp_length - 1
     else:
         # 이진 탐색으로 들어갈 인덱스 반환
         next_index = binary_search(dp_length,num_list[i])
         # 이진 탐색으로 구한 인덱스에 들어온 num_list 값 갱신
-        dp[next_index] = num_list[i]
+        dp[next_index] = (num_list[i],i)
         # 인덱스 별 최대 길이 체크하는 곳에 인덱스 추가
-        dp_array.append(next_index)
-
-ans = []
-
-for i in range(N-1,-1,-1):
-    if dp_array[i] == dp_length-1:
-        ans.append(num_list[i])
-        dp_length -= 1
-ans.reverse()
-print(len(ans))
-print(*ans)
+        path[i] = dp[next_index][1]
+print(dp)
+print(path)
