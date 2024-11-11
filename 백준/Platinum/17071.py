3fc4ef90a2dcb7fcbd598a6c2ec48d229b294399
최근영 <ought119@gmail.com>
2024-11-11 18:54:11 +0900

    17071.py 푸는중

--- 백준/Platinum/17071.py
+++ 백준/Platinum/17071.py
@@ -1,41 +1,68 @@
 from collections import deque
 
+def ValueCheck(value,nextNumber):
+    if 0 <= value <= 500000:
+        if dp[value][0] == False:
+            dp[value][0] = True
+            if (nextNumber[0] + 1) % 2 == 0:
+                dp[value][1][0] = nextNumber[0] + 1
+            else:
+                dp[value][1][1] = nextNumber[0] + 1
+
+            return (nextNumber[0]+1,value)
+        else:
+            if (nextNumber[0] + 1) % 2 == 0:
+                if dp[value][1][0] == 0:
+                    dp[value][1][0] = nextNumber[0] + 1
+                else:
+                    if dp[value][1][0] > nextNumber[0] + 1:
+                        dp[value][1][0] = nextNumber[0] + 1
+            else:
+                if dp[value][1][1] == 0:
+                    dp[value][1][1] = nextNumber[0] + 1
+                else:
+                    if dp[value][1][1] > nextNumber[0] + 1:
+                        dp[value][1][1] = nextNumber[0] + 1
+
 def BFS(start):
     global K
     cnt = 0
-    queue = deque([(start,0)])
+    queue = deque([(0,start)])
     y_check = 0
     while queue:
         next = queue.popleft()
-        if y_check != next[1]:
+        if y_check != next[0]:
             cnt += 1
             K += cnt
-        if K >= 500000:
+
+        if K > 500000:
             return -1
+        
+        if dp[K] == True or K == next[1]:
+            return dp[:K+1]
 
-        if dp[K][0] == True:
-            return dp[:30],cnt,K
+        minous_value = next[1] - 1
+        plus_value = next[1] + 1
+        double_value = next[1] * 2
 
-        if 0 <= next[0]-1 <= 500000 and dp[next[0]-1][0] == False:
-            dp[next[0]-1] = [True,next[1]+1]
-            queue.append((next[0]-1,next[1]+1))
-        if 0 <= next[0]+1 <= 500000 and dp[next[0]+1][0] == False:
-            dp[next[0]+1] = [True,next[1]+1]
-            queue.append((next[0]+1,next[1]+1))
-        if 0 <= next[0]*2 <= 500000 and dp[next[0]*2][0] == False:
-            dp[next[0]*2] = [True,next[1]+1]
-            queue.append((next[0]*2,next[1]+1))
-        y_check = next[1]
+        next_value = ValueCheck(minous_value,next)
+        if next_value != None:
+            queue.append(next_value)
+        next_value = ValueCheck(plus_value,next)
+
+        if next_value != None:
+            queue.append(next_value)
+        next_value = ValueCheck(double_value,next)
+        if next_value != None:
+            queue.append(next_value)
+
+        y_check = next[0]
 
 N, K = map(int,input().split())
 
 if N == K:
     print(0)
 else:
-    dp = [[False,0]] * 500001
-    dp[N] = [True,0]
-    temp = BFS(N)
-    if temp == -1:
-        print(-1)
-    else:
-        print(temp)
+    dp = [[False, [0,0]] for _ in range(500001)]
+    dp[N][0] = True
+    check = BFS(N)
