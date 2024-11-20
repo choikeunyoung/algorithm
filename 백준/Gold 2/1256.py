6ccb7851f522a596046e3d780099f8da9ad30ff5
최근영 <ought119@gmail.com>
2024-11-20 16:24:04 +0900

    1256.py 주석 추가

---
+++ 백준/Gold 2/1256.py
@@ -0,0 +1,32 @@
+def searching(N, M, K):
+    result = []
+    total = N + M
+    # 총 길이가 0보다 클때까지
+    while total > 0:
+        # a의 개수가 0보다 큰 경우 만들 수 있는 조합 식 계산
+        if N > 0:
+            count = comb(N + M - 1, M)
+        else:
+            count = 0
+        # K번 째 수가 위에서 계산산 조합식 count 값보다 작거나 같은 경우 a 로 시작 혹은 끝
+        if K <= count:
+            result.append('a')
+            N -= 1
+        # K번 째 수가 조합식 count 보다 큰 경우 z 로 접근됨
+        else:
+            result.append('z')
+            K -= count
+            M -= 1
+        # 길이 1개 감소
+        total -= 1
+
+    return ''.join(result)
+# combnation 개수 체크
+from math import comb
+
+N, M, K = map(int, input().split())
+
+if K > comb(N + M, M):
+    print(-1)
+else:
+    print(searching(N, M, K))
